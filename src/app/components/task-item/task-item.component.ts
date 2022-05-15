8a5749f2818da574b30df741705d6f9cef377aea
Claudio Nieva <claudionieva.g@gmail.com>
2022-05-15 12:26:26 -0300

    carga de font-awesomme en componente task-item

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import {TASKS} from 'src/app/mock-tasks';
 import { Task } from 'src/app/Task';
 
+import {faTimes} from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
@@ -9,6 +11,9 @@ import { Task } from 'src/app/Task';
 })
 export class TaskItemComponent implements OnInit {
   @Input () task: Task = TASKS[0];
+
+  // FontAwesome
+  faTimes = faTimes;
 
   constructor() { }
 
