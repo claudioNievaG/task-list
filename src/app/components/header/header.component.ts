2dac2cac709dbad964c41c3897c58919c709d012
Claudio Nieva <claudionieva.g@gmail.com>
2022-05-26 08:26:21 -0300

    creando servicio para control de UI

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/service/ui.service'
+import { Subscription } from 'rxjs/internal/Subscription';
 
 @Component({
   selector: 'app-header',
@@ -7,14 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title: string = 'My Task List';
-  // title = 'Nuestra Lista de Tareas';
+  showAddTask: boolean = true;
+  subscription?: Subscription;
 
-  constructor() { }
+  constructor(
+      private uiService: UiService,
+  ) { 
+      this.subscription =  this.uiService.onToggle().subscribe(value => this.showAddTask = value);
+  }
 
   ngOnInit(): void {
   }
 
   toggleAddTask(){
-      console.log("click header");
+      this.uiService.toggleAddTask();
   }
 }
